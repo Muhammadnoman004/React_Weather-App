60349a5317d9852a6547ace093a7ba07b46f2d0f
Noman Shamim <nomanshamim2004@gmail.com>
2024-01-24 15:04:10 +0500

    Still Working

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  let [weather, setweather] = useState([]);
+
+
+  useEffect(function () {
+    getDataFromApi()
+  }, [])
+
+  const getDataFromApi = () => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=karachi&appid=23452a215e99c24e452175b6ff4e8081&units=metric`)
+      .then((res) => res.json())
+      .then((data) => setweather(data.main))
+  }
+
+    console.log(weather.temp);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
