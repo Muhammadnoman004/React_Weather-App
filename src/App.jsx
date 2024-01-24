9f051109ce34b17329d442fa55743d6f1378cdb2
Noman Shamim <nomanshamim2004@gmail.com>
2024-01-24 23:50:04 +0500

    weather render done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
 
   let [weather, setweather] = useState([]);
+  let [inpValue, setinpValue] = useState("");
 
 
   useEffect(function () {
@@ -15,12 +16,23 @@ function App() {
       .then((res) => res.json())
       .then((data) => setweather(data.main))
   }
+  console.log(weather);
+  console.log(weather.temp);
 
-    console.log(weather.temp);
+  const SearchWeather = () => {
+    console.log(inpValue);
+    getDataFromApi(inpValue)
+  }
 
   return (
     <div className="App">
 
+      <div className="main">
+        <input type="text" onChange={(e) => setinpValue(e.target.value)} name="" id="" />
+        <button onClick={SearchWeather}>Search</button>
+        <h2>Temperature {weather.temp}</h2>
+        <h2>Humidity {weather.humidity}</h2>
+      </div>
     </div>
   );
 }
